10a3cd0abebac9cd9d1ad26b25a7dad9bcd9a6c5
masaharu <uchiyama.masaharu999@gmail.com>
2025-05-12 14:38:19 +0000

    2025/5/12

---
+++ atcoder/ABC_362/B/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	xa, ya := ScanI(), ScanI()
+	xb, yb := ScanI(), ScanI()
+	xc, yc := ScanI(), ScanI()
+
+	ab := calD(xa, ya, xb, yb)
+	ac := calD(xa, ya, xc, yc)
+	bc := calD(xb, yb, xc, yc)
+	arr := []int{ab, ac, bc}
+
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	if arr[0]+arr[1] == arr[2] {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func calD(xa, ya, xb, yb int) int {
+	xd := int(math.Abs(float64(xa - xb)))
+	yd := int(math.Abs(float64(ya - yb)))
+
+	return xd*xd + yd*yd
+}
+
+func ScanI() int {
+	sc.Scan()
+	str, _ := strconv.Atoi(sc.Text())
+	return str
+}
+
+func ScanS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ScanIArrayNotBlank() []int {
+	str := strings.Split(ScanS(), "")
+	arrI := make([]int, len(str))
+	for i := 0; i < len(str); i++ {
+		num, _ := strconv.Atoi(str[i])
+		arrI[i] = num
+	}
+
+	return arrI
+}
+
+func ScanIArrayWithBlank(n int) []int {
+	arrI := make([]int, n)
+	for i := 0; i < n; i++ {
+		arrI[i] = ScanI()
+	}
+
+	return arrI
+}
+
+type UnionFind struct {
+	n    int
+	root []int
+}
+
+func NewUnionFind(n int) UnionFind {
+	root := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		root[i] = -1
+	}
+
+	return UnionFind{n, root}
+}
+
+func (uf UnionFind) Leader(x int) int {
+	if uf.root[x] < 0 {
+		return x
+	}
+
+	uf.root[x] = uf.Leader(uf.root[x])
+	return uf.root[x]
+}
+
+func (uf UnionFind) Merge(x, y int) int {
+	xr := uf.Leader(x)
+	yr := uf.Leader(y)
+	if xr == yr {
+		return xr
+	}
+
+	if -uf.root[xr] < -uf.root[yr] {
+		xr, yr = yr, xr
+	}
+	uf.root[xr] += uf.root[yr]
+	uf.root[yr] = xr
+
+	return xr
+}
+
+func (uf UnionFind) Same(x, y int) bool {
+	if uf.Leader(x) == uf.Leader(y) {
+		return true
+	}
+
+	return false
+}
