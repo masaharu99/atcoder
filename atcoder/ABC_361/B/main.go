a19742476bd342de58742eb6fdafcd62cf647a2f
masaharu <uchiyama.masaharu999@gmail.com>
2025-05-13 15:52:25 +0000

    2025/5/14

---
+++ atcoder/ABC_361/B/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	xs1, ys1, zs1, xe1, ye1, ze1 := ScanI(), ScanI(), ScanI(), ScanI(), ScanI(), ScanI()
+	xs2, ys2, zs2, xe2, ye2, ze2 := ScanI(), ScanI(), ScanI(), ScanI(), ScanI(), ScanI()
+
+	if xe1 <= xs2 || xe2 <= xs1 {
+		fmt.Println("No")
+		return
+	}
+	if ye1 <= ys2 || ye2 <= ys1 {
+		fmt.Println("No")
+		return
+	}
+	if ze1 <= zs2 || ze2 <= zs1 {
+		fmt.Println("No")
+		return
+	}
+
+	fmt.Println("Yes")
+}
+
+func ScanI() int {
+	sc.Scan()
+	str, _ := strconv.Atoi(sc.Text())
+	return str
+}
+
+func ScanS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ScanIArrayNotBlank() []int {
+	str := strings.Split(ScanS(), "")
+	arrI := make([]int, len(str))
+	for i := 0; i < len(str); i++ {
+		num, _ := strconv.Atoi(str[i])
+		arrI[i] = num
+	}
+
+	return arrI
+}
+
+func ScanIArrayWithBlank(n int) []int {
+	arrI := make([]int, n)
+	for i := 0; i < n; i++ {
+		arrI[i] = ScanI()
+	}
+
+	return arrI
+}
+
+type UnionFind struct {
+	n    int
+	root []int
+}
+
+func NewUnionFind(n int) UnionFind {
+	root := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		root[i] = -1
+	}
+
+	return UnionFind{n, root}
+}
+
+func (uf UnionFind) Leader(x int) int {
+	if uf.root[x] < 0 {
+		return x
+	}
+
+	uf.root[x] = uf.Leader(uf.root[x])
+	return uf.root[x]
+}
+
+func (uf UnionFind) Merge(x, y int) int {
+	xr := uf.Leader(x)
+	yr := uf.Leader(y)
+	if xr == yr {
+		return xr
+	}
+
+	if -uf.root[xr] < -uf.root[yr] {
+		xr, yr = yr, xr
+	}
+	uf.root[xr] += uf.root[yr]
+	uf.root[yr] = xr
+
+	return xr
+}
+
+func (uf UnionFind) Same(x, y int) bool {
+	if uf.Leader(x) == uf.Leader(y) {
+		return true
+	}
+
+	return false
+}
